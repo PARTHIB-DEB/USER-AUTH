81766d45693cc2dc732f7749ba08fff845cc0e29
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2023-07-01 12:43:31 +0530

    Creating views and templates(front-end)

--- APP/views.py
+++ APP/views.py
@@ -1,19 +1,19 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def signin(request):  # Function for Signing NEW USER
-    return HttpResponse("<h1>SIGN-IN PAGE</h1>")
+    return render(request,"signin.html")
 
 
 def signout(request):  # Function for deleting account / signing out a USER
-    return HttpResponse("<h1>SIGN-OUT PAGE</h1>")
+    return render(request,"signout.html")
 
 
 def login(request):   # Function to give entry permission to AUTHENTICATED USER
-    return HttpResponse("<h1>LOG-IN PAGE</h1>")
+    return render(request,"login.html")
 
 
 def logout(request):  # Function for temporarily disabling one AUTHENTICATED USER's account
-    return HttpResponse("<h1>LOG-OUT PAGE</h1>")
+    return render(request,"logout.html")
 
 
