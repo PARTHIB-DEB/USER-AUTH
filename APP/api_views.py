fa0aadf367d46b09d1f9cb02465e9e3d49484fb9
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2023-07-15 23:00:07 +0530

    Let's start AUTH 2.0

--- APP/api_views.py
+++ APP/api_views.py
@@ -7,15 +7,19 @@ from rest_framework.response import Response
 class UserViewSet(APIView):
     def get(self, request):  # If user want to just see what are the datas
         return Response({"message": "GET request"})
+        
 
     def post(self, request):  # If user wants to post some new data
         return Response({"message": "POST request"})
+        
 
     def put(self, request):  # If user wants to update a complete pack of data
         return Response({"message": "PUT request"})
+        
 
     def patch(self, request):  # If user wants to update just a specific field of a packet of data
         return Response({"message": "PATCH request"})
+        
 
     def delete(self, request):  # If user wants to delete a single field/packet/all packets
         return Response({"message": "DELETE request"})
--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,14 @@ from rest_framework.routers import DefaultRouter
 from APP import api_views
 
 # Create a router and register our viewsets with it.
+
 my_router = DefaultRouter()
-my_router.register(r'', api_views.UserViewSet,basename="user_details")
+my_router.register(r'', api_views.UserViewSet, basename="user_details")
 
 # The API URLs are now determined automatically by the router.
+
 urlpatterns = [
-    path('',api_views.UserViewSet.as_view()),
+    
+    path('', api_views.UserViewSet.as_view()),
+    
 ]
