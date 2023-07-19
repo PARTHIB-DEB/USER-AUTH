59cace87a12bf9ad617c1b017193bb292336ee42
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2023-07-19 09:42:05 +0530

    writing custom validations in update function also

--- APP/serializers.py
+++ APP/serializers.py
@@ -16,34 +16,56 @@ class userSerializer(serializers.ModelSerializer):
         first_name = validated_data.pop('first_name')
         last_name=validated_data.pop('last_name')
         
-        #Writing validations and creating Object of 'User' model
+        # Writing custom validations and creating Object of 'User' model
         
-        if User.objects.filter(email=email).count()==0: #Checking the email
+        try:
+            if User.objects.filter(email=email).count()==0: #Checking the email
                
-            if User.objects.filter(password=password).count()==0: # Checking the password
+                if User.objects.filter(password=password).count()==0: # Checking the password
                    
-                if len(password)>=8: # Verfications for every new password
-                    user_obj=User.objects.create_user(username=username,email=email,password=password)
-                    user_obj.first_name=first_name
-                    user_obj.last_name=last_name
-                    user_obj.save()
-                    return user_obj
+                    if len(password)>=8: # Verfications for every new password
+                        user_obj=User.objects.create_user(username=username,email=email,password=password)
+                        user_obj.first_name=first_name
+                        user_obj.last_name=last_name
+                        user_obj.save()
+                        return user_obj
+                    else:
+                        raise serializers.ValidationError('PASSWORD TOO SHORT')
                 else:
-                    raise serializers.ValidationError('PASSWORD TOO SHORT')
+                    raise serializers.ValidationError('PASSWORD ENTERED BY SOMEONE BEFORE')
             else:
-                raise serializers.ValidationError('PASSWORD ENTERED BY SOMEONE BEFORE')
-        else:
-            raise serializers.ValidationError('EMAIL ENTERED BY SOMEONE BEFORE')
+                raise serializers.ValidationError('EMAIL ENTERED BY SOMEONE BEFORE')
+        except Exception:
+            raise serializers.ValidationError('USERNAME ENTERED BY SOMEONE BEFORE')
 
     def update(self, instance, validated_data): # For updation of an existing 'User' object
-        # updating every/ specific fields (mentioned here only) 
-        instance.username=validated_data.get('username',instance.username) 
-        instance.email = validated_data.get('email', instance.email)
-        instance.password = validated_data.get('password', instance.password)
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.save()
-        return instance
+        
+        # updating every/ specific fields (mentioned here only) with custom validations 
+        username=validated_data.get('username',instance.username)           
+        if User.objects.filter(username=username).count()==0 :
+            
+            email=validated_data.get('email', instance.email)
+            if User.objects.filter(email=email).count()==0: #Checking the email
+               
+                password=instance.password = validated_data.get('password', instance.password)
+                if User.objects.filter(password=password).count()==0: # Checking the password
+                   
+                    if len(password)>=8: # Verfications for every new password
+                        instance.username=validated_data.get('username',instance.username) 
+                        instance.email = validated_data.get('email', instance.email)
+                        instance.password = validated_data.get('password', instance.password)
+                        instance.first_name = validated_data.get('first_name', instance.first_name)
+                        instance.last_name = validated_data.get('last_name', instance.last_name)
+                        instance.save()
+                        return instance
+                    else:
+                        raise serializers.ValidationError('PASSWORD TOO SHORT')
+                else:
+                    raise serializers.ValidationError('PASSWORD ENTERED BY SOMEONE BEFORE')
+            else:
+                raise serializers.ValidationError('EMAIL ENTERED BY SOMEONE BEFORE')
+        else:
+            raise serializers.ValidationError('USERNAME ENTERED BY SOMEONE BEFORE')
 
 
 
