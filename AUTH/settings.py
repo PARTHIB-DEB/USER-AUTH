80649b6af1b1c415bfe84d1319f61c268e7b93c5
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2024-03-04 20:05:28 +0530

    email details hidden

--- AUTH/settings.py
+++ AUTH/settings.py
@@ -131,9 +131,3 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Email Attributes
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'parthibkumardeb@gmail.com'
-EMAIL_HOST_PASSWORD = 'Parthib23!'
